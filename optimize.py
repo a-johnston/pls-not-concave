4a0133073943b6e4e8438191eebcc5e64eec9c25
Adam Johnston <amj69@case.edu>
2017-10-16 20:32:59 -0700

    bug fix. only cut based on largest component of gradient

--- optimize.py
+++ optimize.py
@@ -27,7 +27,7 @@ def optimize(f, n, start=None, block=10, error=1e-9, gradient=_subgradient):
     block size, error, and gradient function which otherwise default to the 0
     point of R^n, 10, 1e-3, and a naive subgradient calculation respectively.
     """
-    X = start or (0,) * n
+    X = list(start or (0,) * n)
     error **= 2
     bounds = [[None, None] for _ in range(n)]
     power = [1 for _ in range(n)]
@@ -38,21 +38,23 @@ def optimize(f, n, start=None, block=10, error=1e-9, gradient=_subgradient):
         if m < error:
             break
 
-        for i, g in enumerate(G):
-            if g >= 0:
-                bounds[i][1] = X[i]
-            if g <= 0:
-                bounds[i][0] = X[i]
-        temp = [0] * n
-        for i in range(n):
-            if bounds[i][0] and bounds[i][1]:
-                temp[i] = (bounds[i][0] + bounds[i][1]) / 2
-            elif bounds[i][0]:
-                temp[i] = X[i] + block ** power[i]
-                power[i] += 1
-            else:
-                temp[i] = X[i] - block ** power[i]
-                power[i] += 1
-        X = tuple(temp)
+        g = max(G, key=lambda x: abs(x))
+        i = G.index(g)
+
+        if g >= 0:
+            bounds[i][1] = X[i]
+        if g <= 0:
+            bounds[i][0] = X[i]
+
+        temp = 0
+        if bounds[i][0] and bounds[i][1]:
+            temp = (bounds[i][0] + bounds[i][1]) / 2
+        elif bounds[i][0]:
+            temp = X[i] + block ** power[i]
+            power[i] += 1
+        else:
+            temp = X[i] - block ** power[i]
+            power[i] += 1
+        X[i] = temp
     return X
 
